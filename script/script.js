d15eec2303eb3b6bffd87ed1f629ac460c8fcfe9
Savcenco-Lucian <154075287+Savcenco-Lucian@users.noreply.github.com>
2023-12-17 18:21:56 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,113 @@
+'use strict';
+
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [overlay, navCloseBtn, navOpenBtn];
+
+/**
+ * close navbar when click on any navbar link
+ */
+
+for (let i = 0; i < navbarLinks.length; i++) { navElemArr.push(navbarLinks[i]); }
+
+/**
+ * addd event on all elements for toggling navbar
+ */
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    elemToggleFunc(navbar);
+    elemToggleFunc(overlay);
+  });
+}
+
+
+
+/**
+ * header active state
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 400 ? header.classList.add("active")
+    : header.classList.remove("active");
+}); 
+
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+
+  for(var i = 0; i < reveals.length; i++){
+
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+function changeValue() {
+  const btnElement = document.querySelector('.send-btn').innerHTML;
+  btnElement.innerHTML = 'Se transmite...';
+  setTimeout(function(){
+    btnElement.innerHTML = 'Transmis!';
+  }, 1000)
+}
+
+function scrollToElement(elementSelector, instance = 0) {
+  // Select all elements that match the given selector
+  const elements = document.querySelectorAll(elementSelector);
+  // Check if there are elements matching the selector and if the requested instance exists
+  if (elements.length > instance) {
+      // Scroll to the specified instance of the element
+      elements[instance].scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");
+
+link1.addEventListener('click', () => {
+  scrollToElement('.about');
+});
+
+link2.addEventListener('click', () => {
+  // Scroll to the second element with "header" class
+  scrollToElement('.service');
+});
+
+link3.addEventListener('click', () => {
+  scrollToElement('.property');
+});
+
+link4.addEventListener('click', () => {
+  scrollToElement('.contacts');
+});
+
